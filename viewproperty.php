f65f39295fd4c7bfbb2250c5cfad6e3e823dd2d7
BeckySawyer <bsawyer1331@gmail.com>
2021-10-30 20:14:39 +1300

    Added very simple base for view property page.

--- viewproperty.php
+++ viewproperty.php
@@ -9,5 +9,34 @@
 
 	<?php require 'partials/nav.php'; ?>
 
+	<div id="main">
+		<div id="gallery">pics</div>
+		<div class="info">
+
+			<h1>Hillcrest, 3216</h1>
+			<h4>2 bedrooms</h4>
+
+			<div class="float-right">
+				<em>$450 per week</em>
+				<button>Book now</button>
+			</div>
+
+			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+		</div>
+	</div>
+
 </body>
 </html>
