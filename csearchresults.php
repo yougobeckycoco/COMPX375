706f71e3fc6407460ff7790019dacc9f9ba8f704
BeckySawyer <bsawyer1331@gmail.com>
2021-10-30 20:09:39 +1300

    Added very simple base for search results page.

--- csearchresults.php
+++ csearchresults.php
@@ -9,5 +9,22 @@
 
 	<?php require 'partials/nav.php'; ?>
 
+	<div id="main">
+		<p>Showing results for destination, date to date, house type, # people, # rooms, $## to $##</p>
+		<a>Sort by low to high</a>
+
+		<div id="results">
+			
+			<div class="result">
+				<img src="http://placehold.it/200">
+				<a href="viewproperty.php">link to result</a>
+				<div class="stats"><h2>Hillcrest, 3216</h2>
+					<p>4 people, 2 bedrooms</p>
+					<price>$450 per week</price>
+			</div>
+
+		</div>
+	</div>
+
 </body>
 </html>
