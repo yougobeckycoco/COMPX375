6ad755e8cc754e18723ac6a8aa0d7b101dceb8cb
BeckySawyer <bsawyer1331@gmail.com>
2021-10-30 18:49:30 +1300

    Base login page and start of client homepage

--- cindex.php
+++ cindex.php
@@ -4,8 +4,23 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NIM Management - Homepage</title>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
 </head>
 <body>
+
+<div id="topbar">
+	
+	<a href="cindex.php">Home</a>
+	<a href="csearch.php">Reservations</a>
+	<a href="#">Messaging</a>
+	<a href="#">My Information</a>
+
+	<button>Log out</button>
+</div>
+<div id="main">
+	
+	<h1>Welcome, first last</h1>
+</div>
 
 </body>
 </html>
--- clogin.php
+++ clogin.php
@@ -4,8 +4,21 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NIM Management - Client Login</title>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
 </head>
 <body>
+
+<div id="section">
+	<form action="cindex.php" method="POST">
+		<label for="email">Enter email</label>
+		<input type="text" name="email" placeholder="Email" id="email">
+		<br>
+		<label for="password">Enter password</label>
+		<input type="password" name="password" placeholder="Password" id="password">
+		<br>
+		<button>Submit</button>
+	</form>
+</div>
 
 </body>
 </html>
