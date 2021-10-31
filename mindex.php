36148bea27131499634151cbe56c5be0051d4ec9
BeckySawyer <bsawyer1331@gmail.com>
2021-11-01 04:47:11 +1300

    Added management pages.

---
+++ mindex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NIM - Admin Homepage</title>
+</head>
+<body>
+
+</body>
+</html>
