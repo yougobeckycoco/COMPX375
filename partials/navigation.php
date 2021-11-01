cc9c3dc9d3fd93e1f4004e62f21042cd1cde9aad
BeckySawyer <bsawyer1331@gmail.com>
2021-11-01 15:58:15 +1300

    Fixed undefined error and added dummy form for review.

---
+++ partials/navigation.php
@@ -0,0 +1,8 @@
+<div id="topbar">
+	
+	<a href="mindex.php">Home</a>
+	<a href="history.php">History of clients</a>
+	<a href="reviewclient.php">Review client</a>
+	<a href="mlogout.php">Log out</a>
+	
+</div>
